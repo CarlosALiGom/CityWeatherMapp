552d0f458dd2f479cff58d42e38f9dcefbd9fe7b
Carlos Aliaga <carlosaligom@gmai.com>
2023-08-13 04:51:39 +0200

    Create WeatherConditions component

--- src/types.ts
+++ src/types.ts
@@ -25,23 +25,13 @@ export interface cityWeatherResponseStructure {
   main: {
     temp: number;
     feels_like: number;
-    temp_min: number;
-    temp_max: number;
-    pressure: number;
     humidity: number;
-  };
-  visibility: number;
-  wind: {
-    speed: number;
-    deg: number;
   };
   clouds: {
     all: number;
   };
   dt: number;
   sys: {
-    type: number;
-    id: number;
     country: string;
     sunrise: number;
     sunset: number;
