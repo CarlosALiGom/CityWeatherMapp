107bb06629fd5c6078f3a84e12abe105370c9dd8
Carlos Aliaga <carlosaligom@gmai.com>
2023-08-13 06:00:51 +0200

    Add NavBar to Layout and weatherCity path on appRouter

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,7 +1,13 @@
 import { Outlet } from "react-router-dom";
+import NavBar from "../NavBar/NavBar";
 
 const Layout = (): React.ReactElement => {
-  return <Outlet />;
+  return (
+    <>
+      <Outlet />
+      <NavBar />
+    </>
+  );
 };
 
 export default Layout;
